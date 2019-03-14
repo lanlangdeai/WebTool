7eb6664cef9b6aeb98cf915e30d204f5b662ad30
lanlang <liaotiam@126.com>
2019-03-14 17:31:18 +0800

    add example

--- src/Common.php
+++ src/Common.php
@@ -32,5 +32,17 @@ class Common
         }
     }
 
+    // 过滤掉emoji表情  
+    function filterEmoji($str)  
+    {  
+        $str = preg_replace_callback(  
+        '/./u',  
+        function (array $match) {  
+            return strlen($match[0]) >= 4 ? '' : $match[0];  
+        },  
+        $str);  
+      
+      return $str;  
+    } 
   
 }
